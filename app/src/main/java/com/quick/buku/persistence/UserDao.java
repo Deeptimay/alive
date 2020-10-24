5141e8d197ef0f7350409a97361f8334b608a356
Deeptimay <deeptimayroutray@gmail.com>
2020-10-24 14:55:40 +0530

    room db setup

---
+++ app/src/main/java/com/quick/buku/persistence/UserDao.java
@@ -0,0 +1,31 @@
+package com.quick.buku.persistence;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.quick.buku.models.Datum;
+
+import java.util.List;
+
+public interface UserDao {
+
+    @Query("SELECT * FROM users")
+    List<Datum> getAll();
+
+    @Query("SELECT * FROM users WHERE firstName LIKE :name LIMIT 1")
+    Datum findByName(String name);
+
+    @Insert
+    void insertAll(List<Datum> users);
+
+    @Insert
+    void insert(Datum user);
+
+    @Update
+    void update(Datum user);
+
+    @Delete
+    void delete(Datum user);
+}
---
+++ app/src/main/java/com/quick/buku/persistence/UserDatabase.java
@@ -0,0 +1,38 @@
+package com.quick.buku.persistence;
+
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
+import com.quick.buku.models.Datum;
+
+@Database(entities = {Datum.class}, version = 1)
+@TypeConverters({Converters.class})
+public abstract class UserDatabase extends RoomDatabase {
+
+    public static final String DATABASE_NAME = "user_db";
+    private static UserDatabase instance;
+
+    public static UserDatabase getInstance(final Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    UserDatabase.class,
+                    DATABASE_NAME
+            ).build();
+        }
+        return instance;
+    }
+
+    public abstract UserDao getUserDao();
+}
+
+
+
+
+
+
